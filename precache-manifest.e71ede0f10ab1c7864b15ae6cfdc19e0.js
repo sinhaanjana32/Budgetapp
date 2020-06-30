37a46163f6fd8644a6d37bbc5a63cf05e9e6a4ea
Anjana <sinhaanjana32@gmail.com>
2020-06-30 13:57:09 +0300

    Updates

---
+++ precache-manifest.e71ede0f10ab1c7864b15ae6cfdc19e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba66b0f07b681a7ed939568a25666fe",
+    "url": "/Budgetapp/index.html"
+  },
+  {
+    "revision": "aee0d96e83856569ffe4",
+    "url": "/Budgetapp/static/css/main.faf498ff.chunk.css"
+  },
+  {
+    "revision": "c0182cffeec831bcf99d",
+    "url": "/Budgetapp/static/js/2.a9d7fc75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Budgetapp/static/js/2.a9d7fc75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aee0d96e83856569ffe4",
+    "url": "/Budgetapp/static/js/main.4f1092a7.chunk.js"
+  },
+  {
+    "revision": "4eaad904fd7b62bec899",
+    "url": "/Budgetapp/static/js/runtime-main.0a6685f5.js"
+  }
+]);
